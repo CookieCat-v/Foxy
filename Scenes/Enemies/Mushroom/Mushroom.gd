11d0baf32e01b0daeebfad13be98cddf5c6a4359
CookieCat-v <phanbaolong1304@gmail.com>
2026-05-18 21:01:51 +0700

    add spike
    add spike, mushroom wip

--- Scenes/Enemies/Mushroom/Mushroom.gd
+++ Scenes/Enemies/Mushroom/Mushroom.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
-
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var speed = 100
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func _physics_process(delta: float) -> void:
+	#simulate gravity
+	velocity.y += gravity * delta
+	#move
+	velocity.x += speed * delta
+	move_and_slide()
