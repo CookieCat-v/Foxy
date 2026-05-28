6407b637797c278ec168b63782c8428c51e7d9f1
KhangFS <khangpham1447@gmail.com>
2026-05-28 17:48:33 +0700

    Add victory scene

--- scripts/hurt_area_2d.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-class_name HurtArea2D
-
-# signal when hurt
-signal hurt(direction: Vector2, damage: float)
-
-# called when take damage
-func take_damage(direction: Vector2, damage: float):
-	hurt.emit(direction, damage)
